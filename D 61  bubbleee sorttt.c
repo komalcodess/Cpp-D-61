32b0e45e93e514828c0f9cb3bec7b96c122393e2
komalcodess <komalkomss02@gmail.com>
2025-10-28 00:40:15 +0530

    Add files via upload

---
+++ D 61  bubbleee sorttt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+int n;
+printf("Enter the number of elements: ");
+scanf("%d",&n);
+int arr[n];
+printf("Enter all the elements: ");
+for(int i=0; i<n; i++){
+scanf("%d",&arr[i]);
+}
+ //for sorting
+for(int i=0; i<n-1; i++){
+for(int j=0; j<n-1-i; j++){
+    if(arr[j]>arr[j+1]){
+        int temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+        }}}
+printf("The sorted elements are:");
+for(int i=0; i<n; i++){
+    printf("%d ",arr[i]);
+}
+
+
+
+
+}
